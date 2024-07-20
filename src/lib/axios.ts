f9c2db68d18c0e58655e7f61d7f1ac2fd753a5cb
Felipe <felipeallanf10@outlook.com>
2024-07-19 22:35:30 -0300

    add baseURL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL : 'http://localhost:3333'
+  baseURL : 'https://api-plann-er-git-main-felipeallanf10s-projects.vercel.app/'
 })
